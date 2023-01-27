d1e7f2e21b6c34ef12bf257b6f70f06bb9470d99
tomoya <shima.apple0941@gmail.com>
2023-01-27 16:32:58 +0900

    first commit

---
+++ js/index.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper(".swiper", {
+  slidesPerView: "auto",
+  spaceBetween: 16,
+  grabCursor: true,  
+  breakpoints: {
+    1025: {
+      spaceBetween: 32,
+    },
+  },
+});

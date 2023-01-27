ea6bd3957f6cf6555d9a5b12ec80fc5f4d11b3e0
tomoya <shima.apple0941@gmail.com>
2023-01-27 23:14:53 +0900

    add header's change color method

--- js/index.js
+++ js/index.js
@@ -1,10 +1,22 @@
+// Swiperの初期化
 const swiper = new Swiper(".swiper", {
   slidesPerView: "auto",
   spaceBetween: 16,
-  grabCursor: true,  
+  grabCursor: true,
   breakpoints: {
     1025: {
       spaceBetween: 32,
     },
   },
+});
+
+// headerの背景色を変更するメソッド
+$(function () {
+  $(window).on("scroll", function () {
+    if ($(".mv").height() < $(this).scrollTop()) {
+      $(".header").addClass("change-color");
+    } else {
+      $(".header").removeClass("change-color");
+    }
+  });
 });
